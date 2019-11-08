fe3b3b6b9e7a35b413d3b209f3447e03ccb69a2f
Kurt Keller <Kurt@pinboard.jp>
2019-11-08 22:40:08 +0100

    transform None to empty strings in QR-code data
    If no extra_infos was given, the value used in the
    actual QR-code was None. To work around this, use
    or '' for all parameters which are not of type address
    
    Somehow the problem does not show up in the tests, but
    only in the QR-code picture itself, which can only be
    seen when actually decoding the QR-code back to
    textual form.

--- qrbill/bill.py
+++ qrbill/bill.py
@@ -200,12 +200,12 @@ class QRBill:
 
     def qr_data(self):
         """Return data to be encoded in the QR code."""
-        values = [self.qr_type, self.version, self.coding, self.account]
+        values = [self.qr_type or '', self.version or '', self.coding or '', self.account or '']
         values.extend(self.creditor.data_list())
         values.extend(self.final_creditor.data_list() if self.final_creditor else [''] * 7)
-        values.extend([self.amount or '', self.currency])
+        values.extend([self.amount or '', self.currency or ''])
         values.extend(self.debtor.data_list() if self.debtor else [''] * 7)
-        values.extend([self.ref_type, self.ref_number or '', self.extra_infos, 'EPD'])
+        values.extend([self.ref_type or '', self.ref_number or '', self.extra_infos or '', 'EPD'])
         return "\r\n".join([str(v) for v in values])
 
     def qr_image(self):
