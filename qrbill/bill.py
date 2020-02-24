a62d70e9e1ff7ed6342c3e73bc747151737f7d40
Kurt Keller <Kurt@pinboard.jp>
2020-02-24 11:57:55 +0100

    switch to python-stdnum v1.13
    python-stdnum v1.13, which contains the additional code we require,
    has now been released, use it and stop using custom code

--- qrbill/bill.py
+++ qrbill/bill.py
@@ -8,6 +8,7 @@ import qrcode.image.svg
 import svgwrite
 from iso3166 import countries
 from stdnum import iban, iso11649
+from stdnum.ch import esr
 
 IBAN_ALLOWED_COUNTRIES = ['CH', 'LI']
 AMOUNT_REGEX = r'^\d{1,9}\.\d{2}$'
@@ -169,8 +170,6 @@ class QRBill:
             # The standard says ultimate creditor is reserved for future use.
             # The online validator does not properly validate QR-codes where
             # this is set, saying it must not (yet) be used.
-            # All the code is there and ready to be used; so simply prevent
-            # users from accessing it.
             raise ValueError("final creditor is reserved for future use, must not be used")
         else:
             self.final_creditor = final_creditor
@@ -191,9 +190,9 @@ class QRBill:
                 self.ref_number = iso11649.validate(ref_number)
             else:
                 raise ValueError("The reference number is invalid")
-        elif len(ref_number) == 27:
+        elif esr.is_valid(ref_number):
             self.ref_type = 'QRR'
-            self.ref_number = ref_number
+            self.ref_number = esr.format(ref_number).replace(" ", "")
         else:
             raise ValueError("The reference number is invalid")
 
@@ -466,12 +465,9 @@ def format_ref_number(bill):
         return ''
     num = bill.ref_number
     if bill.ref_type == "QRR":
-        return ' '.join([
-            num[:2], num[2:7], num[7:12], num[12:17], num[17:22], num[22:]
-        ])
+        return esr.format(num)
     elif bill.ref_type == "SCOR":
-        # In python-stdnum 1.13: return iso11649.format(num)
-        return ' '.join([num[i:i+4] for i in range(0, len(num), 4)])
+        return iso11649.format(num)
     else:
         return num
 
